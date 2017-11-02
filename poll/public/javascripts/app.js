9b208e3622aace999f387d24015db7c5aca05493
Alec Ostheimer <alecostheimer94@gmail.com>
2017-11-02 22:32:00 +0000

    df

--- poll/public/javascripts/app.js
+++ poll/public/javascripts/app.js
@@ -1,1 +1,18 @@
 
+var app = angular.module('app', []);
+app.controller('mainCtrl', function($scope, $http) {
+
+  $scope.addvote = function(OS_system) {
+	if(OS_system == "Windows"){
+          $http.get(Windows_poll)
+	  .then(function(res) {
+ 	    $scope.WindowsVote = res.data;
+	  });  
+  	}
+	if(OS_system == "MacOS"){
+	
+	}
+	if(OS_system == "Linux"){
+  	
+	}
+}

afea0a256c04f801c9c21291084c5aa7297573b8
Alec Ostheimer <alecostheimer94@gmail.com>
2017-11-02 21:24:44 +0000

    Hello

--- poll/routes/index.js
+++ poll/routes/index.js
@@ -2,29 +2,31 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-var OS_poll = {
-  Windows: {
-    name: 'Windows',
-    votes: 0
-  },
-  Linux: {
-    name: 'Linux',
-    votes: 0
-  },
-  MacOS: {
-    name: 'MacOS',
-    votes: 0
-  }
-};
+var OS = ['Windows', 0, 'MacOS',0,'Linux',0]; 
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.sendFile('index.html', {root: 'public'});
 });
 
-router.get('/OS_poll', function(req, res) {
-  console.log("In Poll");
-  res.send(OS_poll);
+router.get('/Windows_poll', function(req, res) {
+  console.log("In Windows Poll"); 
+  OS[1]++;  
+  console.log("Window Votes: " + OS[1]);
+  res.send(OS[1]);
+});
+router.get('/MacOS_poll', function(req,res) {
+  console.log("In MacOS_poll");
+  OS[3]++;
+  console.log("MacOS votes: " + OS[3]);
+  res.send(OS[3]);
+});
+
+router.get('/Linux_poll', function(req,res) {
+  console.log("In Linux_poll");
+  OS[5]++;
+  console.log("Linux votes: " + OS[5]);
+  res.send(OS[5]);
 });
 
 // router.post('/pokemon', function(req, res) {

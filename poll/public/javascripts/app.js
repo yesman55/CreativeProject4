d1b13db7dab14a2c01b08ca9cdbfec48efce3a91
yesman55 <jsi072795@yahoo.com>
2017-11-03 00:06:34 +0000

    new file:   npm-debug.log 	modified:   poll/public/index.html 	modified:   poll/public/javascripts/app.js 	modified:   poll/routes/index.js

--- poll/public/javascripts/app.js
+++ poll/public/javascripts/app.js
@@ -1,18 +1,64 @@
+// var app = window.angular.module('app', []);
+// app.controller('mainCtrl', function($scope, $http) {
 
-var app = angular.module('app', []);
-app.controller('mainCtrl', function($scope, $http) {
-
-  $scope.addvote = function(OS_system) {
-	if(OS_system == "Windows"){
-          $http.get(Windows_poll)
-	  .then(function(res) {
- 	    $scope.WindowsVote = res.data;
-	  });  
-  	}
-	if(OS_system == "MacOS"){
+//   $scope.addvote = function(OS_system) {
+// 	if(OS_system == "Windows"){
+//           $http.get('Windows_poll')
+// 	  .then(function(res) {
+//  	    $scope.WindowsVote = res.data;
+// 	  });
+//   	}
+// 	if(OS_system == "MacOS"){
 	
-	}
-	if(OS_system == "Linux"){
+// 	}
+// 	if(OS_system == "Linux"){
   	
-	}
+// 	}
+// }
+// });
+
+var app = window.angular.module('app', [])
+
+app.factory('osfetcher', osfetcher)
+app.controller('mainCtrl', mainCtrl)
+
+function osfetcher ($http) {
+    var API_ROOT = 'Windows_poll'
+    return {
+        get: function () {
+        return $http
+            .get(API_ROOT)
+            .then(function (resp) {
+            return resp.data
+            })
+        }
+    }
 }
+
+function mainCtrl ($scope, osfetcher, $http, $interval) {
+    // osfetcher.get()
+    // .then(function (data) {
+    //     OS = data
+    // })
+
+    $interval(function () {
+        $http.get("OS")
+        .then(function(response) {
+            $scope.WindowsVotes = response.data[1];
+            $scope.MacOSVotes = response.data[3];
+            $scope.LinuxVotes = response.data[5];
+        });
+        
+    }, 800);
+
+    $scope.addVoteWindows = function() {
+        console.log("Click");
+        $http.get("Windows_poll") 
+        .then(function(response) {
+            $scope.WindowsVotes = response.data;            
+        });
+    }
+    
+    
+}
+
--- poll/routes/index.js
+++ poll/routes/index.js
@@ -2,18 +2,23 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
-var OS = ['Windows', 0, 'MacOS',0,'Linux',0]; 
+var OS = ['Windows', 0, 'MacOS',0,'Linux',0];
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.sendFile('index.html', {root: 'public'});
 });
 
+router.get('/OS', function(req, res) {
+  console.log("In Poll");
+  res.send(OS);
+});
+
 router.get('/Windows_poll', function(req, res) {
   console.log("In Windows Poll"); 
   OS[1]++;  
   console.log("Window Votes: " + OS[1]);
-  res.send(OS[1]);
+  res.send(OS[1].toString());
 });
 router.get('/MacOS_poll', function(req,res) {
   console.log("In MacOS_poll");
@@ -37,3 +42,42 @@ router.get('/Linux_poll', function(req,res) {
 // }); 
 
 module.exports = router;
+
+
+// var express = require('express');
+// var request = require('request');
+// var router = express.Router();
+
+// var OS_poll = {
+//   Windows: {
+//     name: 'Windows',
+//     votes: 0
+//   },
+//   Linux: {
+//     name: 'Linux',
+//     votes: 0
+//   },
+//   MacOS: {
+//     name: 'MacOS',
+//     votes: 0
+//   }
+// };
+
+// /* GET home page. */
+// router.get('/', function(req, res) {
+//   res.sendFile('index.html', {root: 'public'});
+// });
+
+// router.get('/OS_poll', function(req, res) {
+//   console.log("In Poll");
+//   res.send(OS_poll);
+// });
+
+// // router.post('/pokemon', function(req, res) {
+// //     console.log("In Pokemon Post");
+// //     console.log(req.body);
+// //     pokemon.push(req.body);
+// //     res.end('{"success" : "Updated Successfully", "status" : 200}');
+// // }); 
+
+// module.exports = router;
